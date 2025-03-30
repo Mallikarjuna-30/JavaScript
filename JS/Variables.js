c2fd29d465c5c2cb5177c32b1bd858da506610a3
Mallikarjuna-30 <mallikarjunapattar23@gmail.com>
2025-03-30 12:57:00 +0000

    little change

--- JS/Variables.js
+++ JS/Variables.js
@@ -1,1 +1,6 @@
-console.log("HEllo World");
+const account_id=123
+let account_Email="abc@gmail.com"
+var account_Pass="123"
+account_city="ballari"
+
+console.table([account_Email,account_Pass,account_id,account_city])
