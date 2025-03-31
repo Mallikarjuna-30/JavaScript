974edff027ac6be21d3baffbd9612bcd93c33d83
Mallikarjuna-30 <mallikarjunapattar23@gmail.com>
2025-03-31 12:11:34 +0000

    no changes

--- JS/Data_types.js
+++ JS/Data_types.js
@@ -1,8 +1,8 @@
 "use strict"; //treat all JS code as newer version
-// console.log(3+3);
-// let name="MAllikarjuna"
-// let age=18
-// let isLoggedin=false
+console.log(3+3);
+let name="Mallikarjuna"
+let age=18
+let isLoggedin=false
 // number =>2 to power 53
 // string =>""
 // boolean=>true/false
@@ -10,12 +10,11 @@
 // undefined=>
 // symbol=>unique
 
-// obgect
 
-// console.log(typeof "MAllikarjuna");
-// console.log(typeof null);
-// let score = "22"
+console.log(typeof "Mallikarjuna");
+console.log(typeof null);
+let score = "22"
 
-// let valueInNumbre = Number(score)
-// console.log(typeof score);
+let valueInNumbre = Number(score)
+console.log(typeof score);
 
