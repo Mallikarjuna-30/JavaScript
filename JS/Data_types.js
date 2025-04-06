d80f556c7b77e971d3fc44dc1b0e8ae87464fe18
Mallikarjuna-30 <mallikarjunapattar23@gmail.com>
2025-04-06 05:42:11 +0000

    learnt_comparision

--- JS/Data_types.js
+++ JS/Data_types.js
@@ -1,8 +1,8 @@
-"use strict"; //treat all JS code as newer version
-console.log(3+3);
-let name="Mallikarjuna"
-let age=18
-let isLoggedin=false
+// "use strict"; //treat all JS code as newer version
+// console.log(3+3);
+// let name="Mallikarjuna"
+// let age=18
+// let isLoggedin=false
 // number =>2 to power 53
 // string =>""
 // boolean=>true/false
@@ -11,10 +11,28 @@ let isLoggedin=false
 // symbol=>unique
 
 
-console.log(typeof "Mallikarjuna");
-console.log(typeof null);
-let score = "22"
+// console.log(typeof "Mallikarjuna");
+// console.log(typeof null);
+// let score = "22"
 
-let valueInNumbre = Number(score)
-console.log(typeof score);
+// let valueInNumbre = Number(score)
+// console.log(typeof score);
 
+// console.log("2" + 1 +1);
+
+// console.log(true);
+
+// let num1 ,num2, num3 
+// num1 = num2 = num3 =4
+
+// let gameCounter =100
+// ++gameCounter;
+// console.log(gameCounter)
+
+// console.log(null>0);          false
+// console.log(null==0);        false            
+// console.log(null>=0);        true  
+// === 
+
+// console.log("2" == 2);          true
+// console.log("2" === 2);         false 
