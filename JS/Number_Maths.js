d9e0b3dbfe6dfcc014db6425735714c7b3694711
Mallikarjuna-30 <mallikarjunapattar23@gmail.com>
2025-04-10 12:24:36 +0000

    lil_maths

---
+++ JS/Number_Maths.js
@@ -0,0 +1,7 @@
+const score = new Number(400)
+
+// console.log(score.toString().length);
+// console.log(score.toFixed(2));
+
+const value=23.9999452
+console.log(value.toPrecision(4));
--- JS/Strings.js
+++ JS/Strings.js
@@ -1,7 +1,33 @@
 const name="Hello "
 const repCount = 10
 
-console.log(name + repCount + "Value");
+// console.log(name + repCount + "Value");
 
-console.log(`${name} i am mallikarjuna and my repo count is  ${repCount}`);
+// console.log(`${name} i am mallikarjuna and my repo count is  ${repCount}`);
 
+const gameName = new String('mal-lu')
+
+// console.log(gameName[2]);
+// console.log(gameName.length);
+// console.log(gameName.toUpperCase());
+// console.log(gameName.charAt(3));
+// // console.log(gameName.indexOf('a'));
+const newStr = gameName.substring(0,3)
+// console.log(newStr);
+
+const str2=gameName.slice(-1,3)
+// console.log(str2);
+
+// const str3="   mallu   "
+// console.log(str3);
+// console.log(str3.trim());
+
+const url = "https://mallu.com/mallu%20pattar"
+// console.log(url.replace('%20','_'));
+// console.log(url.includes('mallu'))
+
+const str4= "ma-ll-u"
+console.log(str4.split('_'));
+
+
+// learn some more string applications 
