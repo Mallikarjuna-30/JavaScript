18244ba40e6f21d9cb7e2ec0b489ab87e2faccd2
Mallikarjuna-30 <mallikarjunapattar23@gmail.com>
2025-04-16 15:01:52 +0000

    maths done

--- JS/Number_Maths.js
+++ JS/Number_Maths.js
@@ -4,4 +4,20 @@ const score = new Number(400)
 // console.log(score.toFixed(2));
 
 const value=23.9999452
-console.log(value.toPrecision(4));
+console.log(value.toPrecision(5));
+
+
+
+//  MATHS    
+console.log(Math);
+console.log(Math.abs(-10));
+console.log(Math.round(5.5));
+
+console.log(Math.random());   //value lies btw 0 and 1
+console.log(Math.random()*10);
+
+
+const min=10
+const max=20
+
+console.log(Math.round(Math.random()*(max-min+1))+min);
