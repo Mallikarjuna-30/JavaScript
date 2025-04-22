6ac7c21b1d329b1916f9742504306f921d75d922
Mallikarjuna-30 <mallikarjunapattar23@gmail.com>
2025-04-22 10:44:11 +0000

    Arrays

---
+++ JS/Arrays.js
@@ -0,0 +1,21 @@
+// Arrays
+
+const myArr=[2334,1,2,3,4,5,true,"mallu"]
+// console.log(myArr[0]);
+
+// const myHeros=["Ironman", "SpiderMan"]
+
+
+// Array Methods
+// myArr.push(6)
+// myArr.pop()
+// myArr.unshift(1)
+// myArr.shift()
+// console.log(myArr.indexOf(5));
+const newArr=myArr.join()
+// console.log(newArr);
+
+// console.log("A",myArr);
+const myN1=myArr.slice(1,3)
+console.log(myN1);
+
