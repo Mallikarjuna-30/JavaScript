5ffe92f240c152e1c3891e9cdb3c9844887153f4
Mallikarjuna-30 <mallikarjunapattar23@gmail.com>
2025-06-27 08:10:40 +0000

    if conditions done

---
+++ JS/ControlFlow.js
@@ -0,0 +1,77 @@
+// if
+
+const isUserLoggedIn=true
+if(2 === "2"){
+    // console.log("User logged In !!")
+}
+
+const score = 200
+if(score>100){
+    const power = "Fly"
+    // console.log(`user power  = ${power}`)
+}
+// console.log(`user power = ${power}`)
+
+const balace = 1000
+// if (balace>500) console.log("test");                        NOT RECOMMENDED
+
+if (balace < 500) {
+    // console.log("Less than 500 ");
+} else if (balace<750){
+    // console.log("Less than 750");
+    
+}else{
+    // console.log("Above 500");
+    
+}
+
+const UserLoggedIn = true
+const debitCard = false
+if (UserLoggedIn && debitCard){
+    // console.log("User logged in ")
+}
+
+// const month = 3
+// switch (month) {
+    // case 1:
+    //     console.log("Jan")
+    //     break;
+    // case 2:
+    //     console.log("Feb")
+    //     break;
+    // case 3:
+    //     console.log("March")
+    //     break;
+    // case 4:
+    //     console.log("April")
+    //     break;
+
+    // default:
+    //     console.log("None of the above .")
+    //     break;
+// }
+
+
+// const Email = "abc@gamil.com"
+// if(Email.length !== 0){
+//     if(Email){
+//     console.log("Got user email");
+    
+// } else{
+//     console.log("Did not get email");
+    
+// }}
+// else{
+//     console.log("eail is empty");
+    
+// }
+
+// let val;
+// val = 5 ?? 10
+// val=null ?? 10
+// val=undefined ?? 10
+// console.log(val);
+// console.log(val);
+
+const IceTea = 100
+IceTea <= 80 ?console.log("yes"):console.log("no");
