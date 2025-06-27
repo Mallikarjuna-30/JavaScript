204c184712f5b500177f11b05fd236d52e60aaee
Mallikarjuna-30 <mallikarjunapattar23@gmail.com>
2025-06-27 08:35:54 +0000

    for loop

---
+++ JS/Loops.js
@@ -0,0 +1,51 @@
+// Looopss
+
+for (let index = 0; index <= 10; index++) {
+    const element = index;
+    // console.log(element)
+    
+}
+// console.log();
+
+for(i=0;i<10;i++){
+    if(i==5){
+        // console.log("5 is best");
+        
+    }
+    // console.log(i);
+    
+}
+
+for(i=0;i<=5;i++){
+    // console.log(`outer loop : ${i}`);
+    
+    for(j=0;j<=5;j++){
+        // console.log(`inner loop value : ${j} and inner loop ${i}`);
+        // console.log(i+ "*" + j + "=" + i*j);
+        
+    }
+}
+
+let myArray = ["flash " , "Ironman", "batman"]
+
+for (let index = 0; index < myArray.length; index++) {
+    const element = myArray[index];
+    // console.log(element);
+        
+}
+
+for(i=0;i<10;i++){
+    if(i==5){
+        console.log(`detected 5`);
+        break
+    }
+    console.log(`value is ${i}`);
+    
+}
+for(i=0;i<10;i++){
+    if(i==5){
+        continue
+    }
+    console.log(`value is ${i}`);
+    
+}
