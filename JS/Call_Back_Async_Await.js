4c3b5af97277f5ed9f80bf2558861a9c30cadd06
Mallikarjuna-30 <mallikarjunapattar23@gmail.com>
2025-09-07 14:09:13 +0000

    vb

--- JS/Call_Back_Async_Await.js
+++ JS/Call_Back_Async_Await.js
@@ -18,6 +18,31 @@ calculator(1,2,(a,b)=>{
 
 
 const hello = ()=>{
-    console.log("Hello");
+    //console.log("Hello");
 }
 setTimeout(hello,3000)
+
+
+function getData(dataId, getNextData){
+    setTimeout(()=>{
+        console.log("data",dataId);
+        if(getNextData){
+            getNextData();
+        }
+    },2000);
+};
+
+// getData(1,()=>{
+//     console.log("Gettinnggg data 2......");
+    
+//     getData(2), ()=>{
+//             console.log("Gettinnggg data 3......");
+
+//         getData(3,()=>{
+//                 console.log("Gettinnggg data 4......");
+
+//             getData(4);
+//         });
+//     };
+// });
+
