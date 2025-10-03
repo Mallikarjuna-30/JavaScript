3d225a6d5af3a97e4a0c1ebd98bf52389cfc6381
Mallikarjuna-30 <mallikarjunapattar23@gmail.com>
2025-10-03 14:03:15 +0000

    project pending

--- JS/API/api.js
+++ JS/API/api.js
@@ -1,12 +1,40 @@
-const URL="https://jsonplaceholder.typicode.com/users/1";
+const URL = "https://jsonplaceholder.typicode.com/users/1";
 
-const getFacts = async ()=>{
+const getFacts = async () => {
     console.log("Getting Data.....");
     let response = await fetch(URL);
     console.log(response);
     let data = await response.json();
     console.log(data);
-    console.log(data);
+    console.log(data.company.name);
     console.log(response.status);
-    
+
+}
+
+const URL2 = "https://dog.ceo/api/breeds/image/random";
+const btn = document.querySelector('.btn');
+const img = document.querySelector('.imgg');
+const loader = document.querySelector('.loading');
+
+btn.addEventListener('click', (e) => {
+    console.log(e.target);
+    getImg();
+})
+
+const getImg = async () => {
+    loader.style.display='block';
+    img.style.display='none';
+
+
+    console.log("Getting image");
+    let response = await fetch(URL2);
+    //console.log(response);
+    let data = await response.json();
+    //console.log(data);
+    img.src=data.message;
+
+    img.onload = ()=>{
+        loader.style.display='none';
+        img.style.display='block';
+    }
 }
